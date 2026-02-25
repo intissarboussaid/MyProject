baef1459585f4553795d78babcd6c124fa5a8027
intissarboussaid <intissarboussaid2@gmail.com>
2026-02-25 11:51:04 +0100

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ContactComponent } from './contact/contact.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { SkillsComponent } from './skills/skills.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent, AboutMeComponent, SkillsComponent, ProjectsComponent, FooterComponent, NavbarComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'myProject';
+  title = 'potfolio';
+   
+  activeSection = 'header';
+
+  ngAfterViewInit() {
+    const sections = document.querySelectorAll('app-header, app-skills, app-projects, app-contact');
+    const options = { threshold: 0.6 };
+
+    const observer = new IntersectionObserver(entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.activeSection = entry.target.tagName.toLowerCase().replace('app-', '');
+        }
+      });
+    }, options);
+
+    sections.forEach(section => observer.observe(section));
+  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
+import { AppComponent } from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'skills', component: SkillsComponent },
+    { path: 'projects', component: ProjectsComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: '**', component: AppComponent },
+];
